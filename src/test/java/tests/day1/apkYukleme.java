196e4ed412b32498e1633c0b1f321db6c6649060
John <orkun.e@hotmail.com>
2024-03-22 16:30:55 -0400

    Appium 4

--- src/test/java/tests/day1/apkYukleme.java
+++ src/test/java/tests/day1/apkYukleme.java
@@ -1,4 +1,4 @@
-package day01;
+package tests.day1;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -11,22 +11,21 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class apkYukleme {
+    AndroidDriver<AndroidElement> driver;
 
-    AndroidDriver<AndroidElement>driver;
 
     @Test
     public void apkYukleme() throws MalformedURLException {
-
         DesiredCapabilities capabilities=new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 2");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
 
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\orkun\\IdeaProjects\\Appium_t130\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure (1).apk");
-        // App capabilities degeri bir uygulamayi yuklemek icin kullanilan degerdir
-        // Bu capabilities eger ki bir uygulama yuklu degilse onu kontrol eder ve yukler
-        // eger yuklu ise uygulamanin yuklu olup olmadigini kontrol eder ve yukluyse uygulamayi acar
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\Appium_T130\\Apps\\all-currency-converter-3-9-0 (1).apk");
+        // app capabilities degeri bir uygulamayi yuklemek icin kullanilan degerdir.
+        // Bu capability eger ki bir uygulama yukle degilse onu kontrol eder ve yukler eger yuklu ise uygulamainin yuklu olup olmadigini kontrol eder
+        // ve yukluyse uygulamayi acar
 
 
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
--- src/test/java/tests/day2/ArabamAppTest.java
+++ src/test/java/tests/day2/ArabamAppTest.java
@@ -1,4 +1,4 @@
-package day2;
+package tests.day2;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -94,6 +94,11 @@ public class ArabamAppTest {
 
         Assert.assertTrue(Integer.parseInt(enUcuzAracFiyati)>500000);
 
+
+    }
+
+    @Test
+    public void test2(){
 
     }
 
